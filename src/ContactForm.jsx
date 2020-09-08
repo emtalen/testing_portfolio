92ba7d23f2dc9c8094a8ebaf1125a56402680b5e
emtalen <59577366+emtalen@users.noreply.github.com>
2020-09-08 10:52:07 +0200

    adding a debugger to see what thte fuck is going on

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -13,11 +13,13 @@ class ContactForm extends React.Component {
   }
 
   handleSubmit = (e) => {
-    fetch("/", {
+    debugger
+    fetch("/",
+     {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state }),
-    })
+    }) 
       .then(() => alert("Success!"))
       .catch((error) => alert(error));
 
